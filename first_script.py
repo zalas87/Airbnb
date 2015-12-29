07c65d625f72f7a4afd1aee1f749aaf00bd12a88
zalas87 <gonzalorm_1987@hotmail.com>
2015-12-29 18:55:44 +0100

    Add one_hot_encoding, and some represetations

--- first_script.py
+++ first_script.py
@@ -6,6 +6,7 @@ Created on Wed Dec 16 14:26:08 2015
 """
 import pandas as pd
 import numpy as np
+#from sklearn.preprocessing import LabelEncoder
 
 train_data = pd.read_csv("~/Desktop/TFM/input/train_users_2.csv")
 test_data = pd.read_csv("~/Desktop/TFM/input/test_users.csv")
@@ -65,17 +66,27 @@ def add_time_difference(dataframe, st1, st2):
 def add_attrib_totalTimeElapsed(dataframe):
 
     session_data = pd.read_csv("~/Desktop/TFM/input/sessions.csv")
+    session_data['secs_elapsed'].fillna(0, inplace=True)
     #sum up elapsed time for each user
     total_te= session_data.groupby(session_data.user_id, sort=False).sum()
     #merge dataframes
     dataframe = pd.merge(dataframe, total_te, left_on='id', right_on=total_te.index.values, how='left')
     
     return dataframe
+    
+def one_hot_encoder(dataframe, features):
+    for feature in features:
+         dataframe_dummy = pd.get_dummies(dataframe[feature], prefix=feature)
+         dataframe = dataframe.drop([feature], axis=1)
+         dataframe = pd.concat((dataframe, dataframe_dummy), axis=1)   
+
+    return dataframe
 
 
 #Preprocessing the dates
 
-labels = train_data['country_destination'].values
+labels = train_data['country_destination']
+id_test = test_data['id']
 
 train_data = train_data.drop('country_destination',axis=1)
 all_data = pd.concat((train_data,test_data),axis=0, ignore_index=True)
@@ -93,6 +104,81 @@ all_data = add_time_difference(all_data, 'date_account_created', 'timestamp_firs
 #drop some attributes
 all_data = all_data.drop(['affiliate_channel', 'affiliate_provider', 'first_affiliate_tracked', 'signup_app', 'first_device_type', 'first_browser'], axis=1)
 
+#drop id
+all_data = all_data.drop('id', axis=1)
+
+
+
+#Representation
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style="ticks", color_codes=True)
+
+train_users = pd.concat((all_data[:len(labels)],labels),axis=1)
+
+#Plot date_first_booking
+#year
+plt.figure(figsize=(16,10))
+sns.countplot(x=train_users.dfb_year,hue=labels, data=train_users,order=[2010,2011,2012,2013,2014,2015], palette="RdBu")
+sns.despine()
+
+#day
+# cut day values into ranges 
+day_range = pd.cut(train_users["dfb_day"], [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365])
+plt.figure(figsize=(16,10))
+sns.countplot(x=day_range,hue=train_users.country_destination, data=train_users, palette="RdBu")
+sns.despine()
+
+#scatter
+sns.pairplot(train_users, hue="country_destination", markers='o',vars=["dfb_year", "dfb_day"], palette ="RdBu",size=5 )
+
+#Plot gender
+plt.figure(figsize=(16,10))
+sns.countplot(x=train_users['gender'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+sns.despine()
+
+#Plot Signup_method
+plt.figure(figsize=(16,10))
+sns.countplot(x=train_users['signup_method'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+sns.despine()
+
+#Plot Signup_flow
+plt.figure(figsize=(16,10))
+sns.countplot(x=train_users['signup_flow'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+sns.despine()
+
+#Plot Language 
+plt.figure(figsize=(16,10))
+sns.countplot(x=train_users['language'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+sns.despine()
+
+#Plot secs_elapsed --> Don't work
+#plt.figure(figsize=(16,10))
+#sns.countplot(x=train_users['secs_elapsed'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+#sns.despine()
+
+#Plot Age
+# cut age values into ranges 
+age_range = pd.cut(train_users["age"], [0, 14, 24, 34, 44, 54, 64, 74, 84, 94, 104,2020])
+
+plt.figure(figsize=(16,10))
+sns.countplot(x=age_range,hue=train_users.country_destination, data=train_users, palette="RdBu")
+sns.despine()
+
+#Plot dac-tfa
+sns.pairplot(train_users, hue="country_destination", markers='o',vars=["dac-tfa_sec"], palette ="RdBu",size=5 )
+#scatter-years_days_dac-tfa
+sns.pairplot(train_users, hue="country_destination", markers='o',vars=["dfb_year", "dfb_day","dac-tfa_sec"], palette ="RdBu",size=5 )
+
+
+
+
+#One-hot-encoding features
+ohe_feats = ['gender', 'signup_method', 'signup_flow', 'language']
+all_data = one_hot_encoder(all_data, ohe_feats)
+    
+#Classifier   
 X_train = all_data[:len(labels)].values
-y_train = labels
+y_train = labels.values
 X_test = all_data[len(labels):].values

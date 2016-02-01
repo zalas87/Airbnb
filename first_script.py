4d40f5651d371c8ef4c0e0e217f9c93507e03eb6
zalas87 <gonzalorm_1987@hotmail.com>
2016-02-01 23:16:07 +0100

    Add NDCG5 to evaluate the classifiers with a simple data preprocessing

--- first_script.py
+++ first_script.py
@@ -6,10 +6,12 @@ Created on Wed Dec 16 14:26:08 2015
 """
 import pandas as pd
 import numpy as np
-#from sklearn.preprocessing import LabelEncoder
-
-train_data = pd.read_csv("~/Desktop/TFM/input/train_users_2.csv")
-test_data = pd.read_csv("~/Desktop/TFM/input/test_users.csv")
+import scores as scs
+from sklearn.preprocessing import LabelEncoder, Imputer
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.pipeline import Pipeline 
 
 def parse_date(date,c=0):
     import time
@@ -63,10 +65,10 @@ def add_time_difference(dataframe, st1, st2):
     return dataframe
     
 
-def add_attrib_totalTimeElapsed(dataframe):
+def add_attrib_totalTimeElapsed(path, dataframe):
 
-    session_data = pd.read_csv("~/Desktop/TFM/input/sessions.csv")
-    session_data['secs_elapsed'].fillna(0, inplace=True)
+    session_data = pd.read_csv(path + '/input/sessions.csv')
+    #session_data['secs_elapsed'].fillna(0, inplace=True)
     #sum up elapsed time for each user
     total_te= session_data.groupby(session_data.user_id, sort=False).sum()
     #merge dataframes
@@ -82,18 +84,86 @@ def one_hot_encoder(dataframe, features):
 
     return dataframe
 
+def visualization(all_data, labels):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
 
-#Preprocessing the dates
+    sns.set(style="ticks", color_codes=True)
 
-labels = train_data['country_destination']
+    train_users = pd.concat((all_data[:len(labels)],labels),axis=1)
+
+    #Plot date_first_booking
+    #year
+    plt.figure(figsize=(16,10))
+    sns.countplot(x=train_users.dfb_year,hue=labels, data=train_users,order=[2010,2011,2012,2013,2014,2015], palette="RdBu")
+    sns.despine()
+
+    #day
+    # cut day values into ranges 
+    day_range = pd.cut(train_users["dfb_day"], [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365])
+    plt.figure(figsize=(16,10))
+    sns.countplot(x=day_range,hue=train_users.country_destination, data=train_users, palette="RdBu")
+    sns.despine()
+
+    #scatter
+    sns.pairplot(train_users, hue="country_destination", markers='o',vars=["dfb_year", "dfb_day"], palette ="RdBu",size=5 )
+    
+    #Plot gender
+    plt.figure(figsize=(16,10))
+    sns.countplot(x=train_users['gender'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+    sns.despine()
+
+    #Plot Signup_method
+    plt.figure(figsize=(16,10))
+    sns.countplot(x=train_users['signup_method'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+    sns.despine()
+
+    #Plot Signup_flow
+    plt.figure(figsize=(16,10))
+    sns.countplot(x=train_users['signup_flow'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+    sns.despine()
+
+    #Plot Language 
+    plt.figure(figsize=(16,10))
+    sns.countplot(x=train_users['language'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+    sns.despine()
+
+    #Plot secs_elapsed --> Don't work
+    #plt.figure(figsize=(16,10))
+    #sns.countplot(x=train_users['secs_elapsed'],hue=train_users.country_destination, data=train_users, palette="RdBu")
+    #sns.despine()
+
+    #Plot Age
+    # cut age values into ranges 
+    age_range = pd.cut(train_users["age"], [0, 14, 24, 34, 44, 54, 64, 74, 84, 94, 104,2020])
+
+    plt.figure(figsize=(16,10))
+    sns.countplot(x=age_range,hue=train_users.country_destination, data=train_users, palette="RdBu")
+    sns.despine()
+
+    #Plot dac-tfa
+    sns.pairplot(train_users, hue="country_destination", markers='o',vars=["dac-tfa_sec"], palette ="RdBu",size=5 )
+    #scatter-years_days_dac-tfa
+    sns.pairplot(train_users, hue="country_destination", markers='o',vars=["dfb_year", "dfb_day","dac-tfa_sec"], palette ="RdBu",size=5 )
+
+    return 0
+
+    
+    
+ #Loading data
+path = '~/Desktop/TFM'   
+train_data = pd.read_csv(path + '/input/train_users_2.csv')
+test_data = pd.read_csv(path + '/input/test_users.csv')
+labels = train_data['country_destination'].values
 id_test = test_data['id']
 
 train_data = train_data.drop('country_destination',axis=1)
 all_data = pd.concat((train_data,test_data),axis=0, ignore_index=True)
 #all_data = all_data.drop(['id','date_first_booking'], axis=1)
 
+#Preprocessing the dates
 #add the total time elapsed (s) for each user
-all_data  = add_attrib_totalTimeElapsed(all_data)
+all_data  = add_attrib_totalTimeElapsed(path, all_data)
 
 #Parse the column date first booking
 all_data = process_date_columns(all_data, ['date_first_booking'])
@@ -106,79 +176,38 @@ all_data = all_data.drop(['affiliate_channel', 'affiliate_provider', 'first_affi
 
 #drop id
 all_data = all_data.drop('id', axis=1)
-
-
-
-#Representation
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-sns.set(style="ticks", color_codes=True)
-
-train_users = pd.concat((all_data[:len(labels)],labels),axis=1)
-
-#Plot date_first_booking
-#year
-plt.figure(figsize=(16,10))
-sns.countplot(x=train_users.dfb_year,hue=labels, data=train_users,order=[2010,2011,2012,2013,2014,2015], palette="RdBu")
-sns.despine()
-
-#day
-# cut day values into ranges 
-day_range = pd.cut(train_users["dfb_day"], [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365])
-plt.figure(figsize=(16,10))
-sns.countplot(x=day_range,hue=train_users.country_destination, data=train_users, palette="RdBu")
-sns.despine()
-
-#scatter
-sns.pairplot(train_users, hue="country_destination", markers='o',vars=["dfb_year", "dfb_day"], palette ="RdBu",size=5 )
-
-#Plot gender
-plt.figure(figsize=(16,10))
-sns.countplot(x=train_users['gender'],hue=train_users.country_destination, data=train_users, palette="RdBu")
-sns.despine()
-
-#Plot Signup_method
-plt.figure(figsize=(16,10))
-sns.countplot(x=train_users['signup_method'],hue=train_users.country_destination, data=train_users, palette="RdBu")
-sns.despine()
-
-#Plot Signup_flow
-plt.figure(figsize=(16,10))
-sns.countplot(x=train_users['signup_flow'],hue=train_users.country_destination, data=train_users, palette="RdBu")
-sns.despine()
-
-#Plot Language 
-plt.figure(figsize=(16,10))
-sns.countplot(x=train_users['language'],hue=train_users.country_destination, data=train_users, palette="RdBu")
-sns.despine()
-
-#Plot secs_elapsed --> Don't work
-#plt.figure(figsize=(16,10))
-#sns.countplot(x=train_users['secs_elapsed'],hue=train_users.country_destination, data=train_users, palette="RdBu")
-#sns.despine()
-
-#Plot Age
-# cut age values into ranges 
-age_range = pd.cut(train_users["age"], [0, 14, 24, 34, 44, 54, 64, 74, 84, 94, 104,2020])
-
-plt.figure(figsize=(16,10))
-sns.countplot(x=age_range,hue=train_users.country_destination, data=train_users, palette="RdBu")
-sns.despine()
-
-#Plot dac-tfa
-sns.pairplot(train_users, hue="country_destination", markers='o',vars=["dac-tfa_sec"], palette ="RdBu",size=5 )
-#scatter-years_days_dac-tfa
-sns.pairplot(train_users, hue="country_destination", markers='o',vars=["dfb_year", "dfb_day","dac-tfa_sec"], palette ="RdBu",size=5 )
-
-
-
+    
+#Visualization
+#visualization(all_data, labels)
 
 #One-hot-encoding features
 ohe_feats = ['gender', 'signup_method', 'signup_flow', 'language']
 all_data = one_hot_encoder(all_data, ohe_feats)
+  
+#replace NaN values -> skip error Scikit-learn  
+all_data = all_data.fillna(-1)
+
+#Splitting train and test
+values = all_data.values
+#X_train = values[:len(labels)]
+X_train = values[:len(labels)-40000]
+Xtc = values[len(labels)-40000: len(labels)]
+X_test = values[len(labels):]
+#mlb = MultiLabelBinarizer(classes=("AU","CA","DE","ES","FR","GB","IT","NDF","NL","PT","US","other"))
+#labels.shape = len(labels),1
+le = LabelEncoder()
+Y_train = le.fit_transform(labels)
+Ytc = Y_train[len(labels)-40000:]
+Y_train = Y_train[:len(labels)-40000] 
+X_test = values[len(labels):]
+   
+#Classifier Random Forest
+clf = RandomForestClassifier(n_estimators=25, max_depth=6)
+clf = clf.fit(X_train, Y_train)
+y_pred = clf.predict(X_test)
+y_predictions_test=clf.predict_proba(X_test)
     
-#Classifier   
-X_train = all_data[:len(labels)].values
-y_train = labels.values
-X_test = all_data[len(labels):].values
+predictionsRF = clf.predict_proba(Xtc)
+
+scoreRF = scs.score_ndcg5(predictionsRF, Ytc, 5)
+print scoreRF
---
+++ scores.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 20:06:04 2016
+
+@author: gonzalo
+"""
+
+import numpy as np
+
+def dcg_at_k(r, k, method=0):
+    """Score is discounted cumulative gain (dcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per.pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> dcg_at_k(r, 1)
+    3.0
+    >>> dcg_at_k(r, 1, method=1)
+    3.0
+    >>> dcg_at_k(r, 2)
+    5.0
+    >>> dcg_at_k(r, 2, method=1)
+    4.2618595071429155
+    >>> dcg_at_k(r, 10)
+    9.6051177391888114
+    >>> dcg_at_k(r, 11)
+    9.6051177391888114
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+        k: Number of results to consider
+        method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307, ...]
+                If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+        Discounted cumulative gain
+    """
+    r = np.asfarray(r)[:k]
+
+    if r.size:
+        if method == 0:
+            return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+        elif method == 1:
+            gain = 2**r-1
+            discount = np.log2(np.arange(2, r.size + 2))
+            #return np.sum(r / np.log2(np.arange(2, r.size + 2))) -> the same
+            return np.sum(gain/discount) #No entiendo porque cuenta en 2
+        else:
+            raise ValueError('method must be 0 or 1.')
+    return 0.
+
+
+def ndcg_at_k(r, k, method=0):
+    """Score is normalized discounted cumulative gain (ndcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per.pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> ndcg_at_k(r, 1)
+    1.0
+    >>> r = [2, 1, 2, 0]
+    >>> ndcg_at_k(r, 4)
+    0.9203032077642922
+    >>> ndcg_at_k(r, 4, method=1)
+    0.96519546960144276
+    >>> ndcg_at_k([0], 1)
+    0.0
+    >>> ndcg_at_k([1], 2)
+    1.0
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+        k: Number of results to consider
+        method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307, ...]
+                If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+        Normalized discounted cumulative gain
+    """
+    dcg_max = dcg_at_k(sorted(r, reverse=True), k, method)
+    if not dcg_max:
+        return 0.
+    return dcg_at_k(r, k, method) / dcg_max
+
+
+
+def score_ndcg5(preds, ground_truth, k=5):
+    """
+    preds : ndarray, shape = [n_samples, n_classes]
+        Predicted probabilities.
+    ground_truth : ndarray, shape = [n_samples]
+        Ground_truth (true labels represended as integers)..
+    """
+    assert(preds.shape[0]==ground_truth.shape[0])
+    
+    scores = []
+    
+    rows = preds.shape[0]
+    r = np.zeros((rows,preds.shape[1]))
+    
+    preds_sorted = np.argsort(preds)[:,::-1]  
+    ground_truth.shape = rows,1 #Unidimensional label
+    r[preds_sorted==ground_truth] = 1
+
+    for y_score in r:
+        score = ndcg_at_k(y_score,k=5,method=1)
+        scores.append(score)
+    
+    return np.mean(scores)
+    
+    
+#preds = np.array([[0.34,0.22,0.,0.44],[0,0.8,0.05, 0.15],[0,0.3,0.34,0.36]])
+#truth = np.array([1,1,2])
+#print('predictions: \n', preds)
+#print('\n\n truth: \n', truth)
+#print('\n\n scores: \n', score_ndcg5(preds, truth))
